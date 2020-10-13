d7a6df722d4d7cee553a5b365851e087b577cc65
Aline Costa <linecosta04@gmail.com>
2020-10-13 17:42:04 -0300

    modificando consulta c

--- queries.sql
+++ queries.sql
@@ -6,32 +6,26 @@ WHERE creditos = 3 and nome_dept ='Dep. Ciência da Computação'
 
 ------------b) Achar a matrícula de cada seção que foi oferecida no outono de 2019.
 
-
 SELECT id_secao
 FROM secao
 WHERE semestre = 'outono' and ano = '2019'
 
 ------------c) Achar as seções que tiveram o número máximo de matriculados no outono de 2019.
 
-
-select cod_secao
-from realiza 
-group by cod_secao 
-order by count(cod_secao) desc limit 1
-
-
+select count(r.cod_secao) as qtd_alunos_matriculados, r.cod_secao, s.capacidade as capacidade_sala
+from realiza as r join secao as sec on r.cod_secao = sec.id_secao join sala as s on sec.numero_sala = s.numero_sala
+where sec.semestre = 'outono'and sec.ano = '2019'
+group by r.cod_secao, s.capacidade
+having count(r.cod_secao) = s.capacidade
 
 ------------d) Ache os pontos totais recebidos pelo aluno de ID 12345, por todos os cursos realizados por ele.
-
 
 select sum(r.nota), al.nome
 from aluno as al join realiza as r on al.id_aluno = r.cod_aluno
 where al.id_aluno = 12345
 group by al.nome
 
-
 ------------e) Ache a média de pontos (coeficiente de rendimento) para este mesmo aluno, ou seja, o total de pontos dividido pelo total de créditos para os cursos associados.
-
 
 select (sum(r.nota)/sum(c.creditos)), al.nome
 from aluno as al join realiza as r on al.id_aluno = r.cod_aluno join curso as c on c.id_curso = r.cod_curso
